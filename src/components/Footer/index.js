9bad9f960f4c67b690b80f7c6389cf126adc7907
Fabio Leon <fsleon93@gmail.com>
2023-11-23 19:04:47 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,46 +2,83 @@ import React from "react";
 import "./styles.css";
 
 const Footer = () => {
-    return (
-        <div className="main-footer">
-      <div className="container">
-        <div className="row">
-          {/* Column1 */}
-          <div className="col">
-            <h4>THICC MEMES INC</h4>
-            <h1 className="list-unstyled">
-              <li>342-420-6969</li>
-              <li>Moscow, Russia</li>
-              <li>123 Streeet South North</li>
-            </h1>
+  return (
+    <div>
+      <main>
+        {/* Content Goes Here */}
+      </main>
+      <footer>
+        <div id="footer_content">
+          <div id="footer_contacts">
+            <h1>Logo</h1>
+            <p>It's all about your dreams.</p>
+
+            <div id="footer_social_media">
+              <a href="#" className="footer-link" id="instagram">
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+
+              <a href="#" className="footer-link" id="facebook">
+                <i className="fa-brands fa-facebook-f"></i>
+              </a>
+
+              <a href="#" className="footer-link" id="whatsapp">
+                <i className="fa-brands fa-whatsapp"></i>
+              </a>
+            </div>
           </div>
-          {/* Column2 */}
-          <div className="col">
-            <h4>Stuff</h4>
-            <ui className="list-unstyled">
-              <li>DANK MEMES</li>
-              <li>OTHER STUFF</li>
-              <li>GUD STUFF</li>
-            </ui>
-          </div>
-          {/* Column3 */}
-          <div className="col">
-            <h4>WELL ANOTHER COLUMN</h4>
-            <ui className="list-unstyled">
-              <li>DANK MEMES</li>
-              <li>OTHER STUFF</li>
-              <li>GUD STUFF</li>
-            </ui>
+
+          <ul className="footer-list">
+            <li>
+              <h3>Usuário</h3>
+            </li>
+            <li>
+              <a href="#" className="footer-link">Paciente</a>
+            </li>
+            <li>
+              <a href="#" className="footer-link">Blog</a>
+            </li>
+            <li>
+              <a href="#" className="footer-link">Sobre</a>
+            </li>
+          </ul>
+
+          <ul className="footer-list">
+            <li>
+              <h3>Produtos</h3>
+            </li>
+            <li>
+              <a href="#" className="footer-link">App</a>
+            </li>
+            <li>
+              <a href="#" className="footer-link">Desktop</a>
+            </li>
+            <li>
+              <a href="#" className="footer-link">Cloud</a>
+            </li>
+          </ul>
+
+          <div id="footer_subscribe">
+            <h3>Contato</h3>
+
+            <p>
+              Informe seu e-mail e entraremos em contato.
+            </p>
+
+            <div id="input_group">
+              <input type="email" id="email" />
+              <button>
+                <i className="fa-regular fa-envelope"></i>
+              </button>
+            </div>
           </div>
         </div>
-        <hr />
-        <div className="row">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} THICC MEMES | All rights reserved |
-            Terms Of Service | Privacy
-          </p>
+
+        <div id="footer_copyright">
+          &#169;
+          2023 all rights reserved
         </div>
-      </div>
+      </footer>
     </div>
   );
 };
