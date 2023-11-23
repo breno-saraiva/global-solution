ff22427ada0a73f1c2edf49aaefc9bfac9411f7e
LucasMazer <joaolucasmazer@gmail.com>
2023-11-22 21:29:13 -0300

    Navbar + Pacientes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,19 +3,31 @@ import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import useAuth from "../../hooks/useAuth";
 import * as C from "./styles";
+import Navbar from "../../components/Navbar";
+import CardPaciente from "../../components/CardPaciente";
 
 const Home = () => {
   const { signout } = useAuth();
   const navigate = useNavigate();
 
   return (
+      <div>
+        
     <C.Container>
-      <C.Title>Home</C.Title>
-      <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
-        Sair
-      </Button>
+      <Navbar/>
     </C.Container>
+  
+    <C.Card>
+      <CardPaciente/>
+    </C.Card>
+
+      </div> 
+
   );
 };
 
 export default Home;
+
+{/* <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+  Sair
+</Button> */}
