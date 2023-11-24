ebd8e324f2a9472e2870b9b47997f380b3980ebe
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-11-23 21:50:58 -0300

    add modal

---
+++ src/components/modal/index.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+export function Modal (props) {
+    if(props.isOpen) {
+        return (
+            <div className="background">
+                <div className="modal">
+                    <h1>
+                        criando um Modal
+                    </h1>
+                    <p>tentando criar algo</p>
+                    <button onClick={props.showModal}>fechar</button>
+                </div>
+            </div>
+        )
+    }
+}
